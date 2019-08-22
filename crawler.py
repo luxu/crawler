9a84c640a3a2348632449c346c6dd8fb2ca63bf9
Lhaís Rodrigues <lhaisrs@gmail.com>
2019-08-22 10:46:29 -0300

    crawler americanas

--- crawler.py
+++ crawler.py
@@ -1,27 +1,66 @@
 import requests
 import csv
 from bs4 import BeautifulSoup
+import pandas as pd
 
-pages = []
+f = csv.writer(open('americanas.csv', 'w'))
+f.writerow(['Logradouro',
+            'Endereço',
+            'Numero',
+            'Complemento',
+            'Bairro',
+            'Cidade',
+            'Estado',
+            'Região',
+            'País',
+            'CEP',
+            'Endereço completo',
+            'Latitude',
+            'Longitude'])
 
-f = csv.writer(open('z-artist-names.csv', 'w'))
-f.writerow(['Name', 'Link'])
+url = 'https://ri.lasa.com.br/lojas?all'
 
-for i in range(1, 5):
-	url = 'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ' + str(i) + '.htm'
-	pages.append(url)
+page = requests.get(url)
+soup = BeautifulSoup(page.text, 'html.parser')
 
-for url in pages:
-	page = requests.get(url)
-	soup = BeautifulSoup(page.text, 'html.parser')
+store_list = soup.find_all(class_='lojas')[2]
 
-	last_links = soup.find(class_='AlphaNav')
-	last_links.decompose()
+store_list_state_items = store_list.find_all('h3')
+fields_store_items = store_list.find_all('ul')
 
-	artist_name_list = soup.find(class_='BodyText')
-	artist_name_list_items = artist_name_list.find_all('a')
+# Values
+states = []
+names = []
+store_state = []
+cities = []
+addresses = []
 
-	for artist_name in artist_name_list_items:
-		names = artist_name.contents[0]
-		links = 'https://web.archive.org' + artist_name.get('href')
-		f.writerow([names, links])
+
+for state in store_list_state_items:
+    state_name = state.contents[1]
+    states.append(state_name)
+
+for field in fields_store_items:
+    store_name_items = field.contents[3].find_all(class_='name')
+    store_state.append(len(store_name_items))
+    store_city_items = field.contents[3].find_all(class_='cidade')
+    values_store_items = field.contents[3].find_all(class_='col-md-10')
+    for name in store_name_items:
+        store_name = name.text
+        names.append(store_name)
+    for city in store_city_items:
+        store_city = city.text
+        cities.append(store_city)
+    for value in values_store_items:
+        address = value.contents[3].text
+        addresses.append(address)
+
+for el in store_state:
+    index = store_state.index(el)
+    state = states[index]
+    for value in range(el):
+        name = names[value]
+        city = cities[value]
+        address = addresses[value]
+        f.writerow(['', '', '', '', '', city, state,
+                    '', 'Brasil', '', address, '', ''])
